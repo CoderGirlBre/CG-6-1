039a8c1a45ce0570b499c40ba15bd868848163a6
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-08-19 00:21:28 -0500

    Add files via upload

--- FullName2/Program.cs
+++ FullName2/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-
+using System.Text;
 namespace FullName2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Asks the user for their first name and assigns it to firstName variable.
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            //Asks user their last name and assigns it to lastName variable.
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            //New stringbuilder called mystring that concatenates the literal string
+            //"full name" with the method created below, which print the first
+            //and last name with a space in between.
+            StringBuilder myString = new StringBuilder();
+
+            myString.Append("Full name: ");
+            myString.Append(Welcome(firstName, lastName));
+
+            Console.WriteLine(myString); //Printing the string to the console.
+            Console.ReadLine();
+        }//New method.
+        private static string Welcome(string firstName, string lastName)
+        {//This prints the first and last name to the console with a space in between.
+
+            return firstName + " " + lastName;
+
         }
+    
     }
 }
